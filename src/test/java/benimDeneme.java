f9a83a24f3b2a085746a7e0f050ecbe75ed7d50f
onurmumcu <onur.mmc@gmail.com>
2019-07-08 19:14:11 -0400

    hp 55.yolla

--- src/test/java/benimDeneme.java
+++ src/test/java/benimDeneme.java
@@ -1,4 +1,5 @@
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Iterator;
 
@@ -8,12 +9,24 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class benimDeneme {
+	
+	FileInputStream fis;
+	XSSFWorkbook workbook;
+	
+	
 	public static void main(String[] args) throws IOException {
 		
-		FileInputStream fis=new FileInputStream("demoData.xlsx");
 		
-		XSSFWorkbook workbook = new XSSFWorkbook(fis);
 		
+		 	
+	}
+
+	
+	
+	public  void butunSheetYazdir() throws IOException {
+		
+		fis=new FileInputStream("demoData.xlsx");
+		workbook = new XSSFWorkbook(fis);
 		int sheetNum = workbook.getNumberOfSheets();
 		
 		for(int i =0; i<sheetNum; i++) {
@@ -39,37 +52,21 @@ public class benimDeneme {
 				
 				System.out.println();
 	}	
-//				Row secondRow = rows.next();
-//				
-//				Iterator<Cell> secondRowCells = secondRow.cellIterator();
-//				
-//
-//				
-//
-//				while(secondRowCells.hasNext()) {
-//				Cell secondCell = secondRowCells.next();
-//				
-//				System.out.print(secondCell+" | ");
-//				}
-//				
-				
-//				Cell secondCell = cells.next();
-//				System.out.print(secondCell+" | ");
-				
-//				if(firstCell.getStringCellValue().equalsIgnoreCase("data1")) {
-//					System.out.println(firstCell);
-//				}
-					
-				
-				
+
 				
 			}
-			
+		}
+		}
 		
-			
-			}
-		
+	
+
+	public  void butunRowYazdir() throws IOException {
+		fis=new FileInputStream("demoData.xlsx");
+		workbook = new XSSFWorkbook(fis);
 		
 	}
 
+	
+	
+	
 }
